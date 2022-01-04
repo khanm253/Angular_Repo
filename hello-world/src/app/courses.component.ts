0a68794cbe082667ab26d28b9bfc5da46511dc49
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2022-01-04 16:49:41 -0500

    Learning component generation.

---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'courses',
+    template: '<h2>{{ getTitle() }}</h2>',
+
+})
+export class CoursesComponent{
+    title = "List of Courses"
+
+    getTitle(){
+        return this.title
+    }
+}
