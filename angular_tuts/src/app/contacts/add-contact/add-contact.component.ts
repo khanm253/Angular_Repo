5648b6bdeb0e844b6968b702b2281aa0ddef81a9
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2022-01-08 20:16:10 -0500

    Explored lifecycle methods

---
+++ angular_tuts/src/app/contacts/add-contact/add-contact.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-add-contact',
+  templateUrl: './add-contact.component.html',
+  styleUrls: ['./add-contact.component.scss']
+})
+export class AddContactComponent implements OnInit, OnChanges, OnDestroy {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+      
+  }
+
+  ngOnDestroy(): void {
+      
+  }
+
+}
--- angular_tuts/src/app/contacts/contacts.module.ts
+++ angular_tuts/src/app/contacts/contacts.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditComponentComponent } from './edit-component/edit-component.component';
+import { DeleteComponentComponent } from './delete-component/delete-component.component';
+import { ListComponentComponent } from './list-component/list-component.component';
+import { ViewComponentComponent } from './view-component/view-component.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddContactComponent,
+    EditComponentComponent,
+    DeleteComponentComponent,
+    ListComponentComponent,
+    ViewComponentComponent
+  ],
   imports: [
     CommonModule
   ]
