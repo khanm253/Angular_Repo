a6c008b3c1f45bee7c7d3cffef2beb8e341fc0a8
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2022-01-12 12:05:26 -0500

    Learning angular routes: Guards

--- angular_tuts/src/app/app-routing.module.ts
+++ angular_tuts/src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { ClientComponent } from './client/client.component';
 import { ProductComponent } from './product/product.component';
 import { SearchComponent } from './search/search.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  {path:'product/:id', component: ProductComponent},
+  {path:'product/:id', component: ProductComponent, canActivate: [AuthGuard]},
   {path:'client', component: ClientComponent},
   {path:'search', component: SearchComponent},
   { path: 'payments', loadChildren: () => import('./payments/payments.module').then(m => m.PaymentsModule) },
