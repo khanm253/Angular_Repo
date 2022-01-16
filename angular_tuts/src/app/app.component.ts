21268f299cc3b4a15ffdc49f658145b5ca71dffd
Abdullah Khan <mohammedibne.khan@mail.utoronto.ca>
2022-01-16 15:58:53 -0500

    FNISHING UP

--- angular_tuts/src/app/app.component.ts
+++ angular_tuts/src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+
   title = 'angular_tuts';
   success = true;
   super = "wonder";
   percent = 0.69
   date = Date.now()
+  // @ViewChild('headline')
+  // headline: ElementRef;
+
   contacts = [{
     name: 'Abd',
     id: 1
@@ -24,6 +28,11 @@ export class AppComponent {
 
   sayHello():void{
     console.log('Hello')
+  }
+
+    
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
   }
 
 
